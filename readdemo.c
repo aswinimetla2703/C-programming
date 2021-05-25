e86624a45bf1c5eed483f451f8b0f82b50218a01
Aswini Metla <84316487+aswinimetla2703@users.noreply.github.com>
2021-05-25 06:34:22 -0700

    Create readdemo.c

---
+++ readdemo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int input;
+  clrscr();
+  printf("please enter input:\n");
+  scanf("%d",&input);
+  printf("the input value is:\n");
+  printf("%d",input);
+  return 0;
+}
